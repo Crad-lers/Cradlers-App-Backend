50659b89b08d901013fa85c4c60b3355f87bc7f3
Gagana-Chandupa <gaganavid.chandupa@gmail.com>
2025-03-08 00:34:37 +0530

    Implement API to add new music tracks

--- routes/musicRoutes.js
+++ routes/musicRoutes.js
@@ -25,4 +25,21 @@ router.post("/stop", protect, (req, res) => {
   res.json({ message: "Music stopped" });
 });
 
+router.post("/add", protect, async (req, res) => {
+    const { name, url } = req.body;
+  
+    if (!name || !url) {
+      return res.status(400).json({ message: "Track name and URL are required" });
+    }
+  
+    try {
+      const newTrack = new MusicTrack({ name, url, userId: req.user.id });
+      await newTrack.save();
+  
+      res.status(201).json({ message: "Track added successfully", track: newTrack });
+    } catch (error) {
+      res.status(500).json({ message: "Server error" });
+    }
+  });
+
 module.exports = router;
